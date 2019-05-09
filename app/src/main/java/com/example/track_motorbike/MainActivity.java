c21b2e6d622ffcae4316d72101c9b137b4aaae03
Wanjiru Catherine <catherinemunene802@gmail.com>
2019-05-09 21:51:59 +0300

    Added fragments

--- app/src/main/java/com/example/track_motorbike/MainActivity.java
+++ app/src/main/java/com/example/track_motorbike/MainActivity.java
@@ -2,44 +2,63 @@ package com.example.track_motorbike;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_track:
-                    mTextMessage.setText(R.string.title_track);
-                    return true;
-                case R.id.navigation_activate:
-                    mTextMessage.setText(R.string.title_activate);
-                    return true;
-                case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
-                    return true;
-                case R.id.navigation_secure:
-                    mTextMessage.setText(R.string.title_secure);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        BottomNavigationView navView = findViewById(R.id.navigation_view);
+        navView.setOnNavigationItemSelectedListener(this);
+
+        LoadFragment(new TrackFragment());
     }
 
+    private boolean LoadFragment(Fragment fragment){
+        if(fragment != null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container,fragment)
+                    .commit();
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()){
+
+            case R.id.navigation_activate:
+                fragment = new ActivateFragment();
+                break;
+
+            case R.id.navigation_track:
+                fragment = new TrackFragment();
+                break;
+
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+
+            case R.id.navigation_secure:
+                fragment = new SecureFragment();
+                break;
+        }
+
+        return LoadFragment(fragment);
+    }
 }
