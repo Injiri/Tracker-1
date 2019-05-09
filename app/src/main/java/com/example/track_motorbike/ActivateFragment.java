f3e7f628cc9f5b0635bee5ace35430e98b4320d9
Wanjiru Catherine <catherinemunene802@gmail.com>
2019-05-09 22:10:26 +0300

    Added logic for Activate Functionality

--- app/src/main/java/com/example/track_motorbike/ActivateFragment.java
+++ app/src/main/java/com/example/track_motorbike/ActivateFragment.java
@@ -7,12 +7,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class ActivateFragment extends Fragment {
+public class ActivateFragment extends Fragment implements View.OnClickListener {
+
+
+    Button activate;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_activate, null);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
+
+public class fragmentOne extends Fragment implements  {
+    Button myButton;
+
+    @Override
+    public View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedinstanceState) {
+        View myView = inflater.inflate(R.layout.fragment_1, container, false);
+        myButton = (Button) myView.findViewById(R.id.myButton);
+        myButton.setOnClickListener(this);
+        return myView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // implements your things
     }
 }
