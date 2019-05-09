aad22502abad5b979cb038b71fe30b2cef20c4ba
Wanjiru Catherine <catherinemunene802@gmail.com>
2019-05-09 21:07:05 +0300

    First commit

---
+++ app/src/main/java/com/example/track_motorbike/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.track_motorbike;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_track:
+                    mTextMessage.setText(R.string.title_track);
+                    return true;
+                case R.id.navigation_activate:
+                    mTextMessage.setText(R.string.title_activate);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+                case R.id.navigation_secure:
+                    mTextMessage.setText(R.string.title_secure);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
