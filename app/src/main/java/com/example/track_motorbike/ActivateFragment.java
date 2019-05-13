4df9dd58f33b9f9d59e77efbaca141bf7455c11b
Wanjiru Catherine <catherinemunene802@gmail.com>
2019-05-13 18:08:51 +0300

    Fixed

--- app/src/main/java/com/example/track_motorbike/ActivateFragment.java
+++ app/src/main/java/com/example/track_motorbike/ActivateFragment.java
@@ -5,7 +5,6 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
@@ -45,40 +44,10 @@ public class ActivateFragment extends Fragment implements View.OnClickListener {
      }
 
     public void sendSMS() {
+
             SmsManager sms = SmsManager.getDefault();
             String msg = "Activate";
             sms.sendTextMessage("0701869953", null, msg, null, null);
-
-     if (ContextCompat.checkSelfPermission(this,
-    Manifest.permission.SEND_SMS)
-            != PackageManager.PERMISSION_GRANTED) {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.SEND_SMS)) {
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.SEND_SMS},
-                    MY_PERMISSIONS_REQUEST_SEND_SMS);
-        }
-    }
-}
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage(phoneNo, null, message, null, null);
-                    Toast.makeText(getApplicationContext(), "SMS sent.",
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(),
-                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
-                    return;
-                }
-            }
-        }
 
     }
 
